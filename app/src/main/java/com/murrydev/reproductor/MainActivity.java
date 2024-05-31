c51771a26fa677c1230e5852eba2b9a714e08d7e
RafaelMurrieta <r7121005846@gmail.com>
2024-05-31 09:47:51 -0600

    Funciona el reproductor

--- app/src/main/java/com/murrydev/reproductor/MainActivity.java
+++ app/src/main/java/com/murrydev/reproductor/MainActivity.java
@@ -2,8 +2,10 @@ package com.murrydev.reproductor;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,5 +42,65 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+    }
+    public void PLayPause(View view){
+        if (vectormp[posicion].isPlaying()){
+            vectormp[posicion].pause();
+            btnplay_pause.setBackgroundResource(R.drawable.play);
+            Toast.makeText(this, "Pause", Toast.LENGTH_LONG).show();
+        }else{
+            vectormp[posicion].start();
+            btnplay_pause.setBackgroundResource(R.drawable.pause);
+            Toast.makeText(this, "Play", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void stop(View view){
+        if (vectormp[posicion] != null){
+            vectormp[posicion].stop();
+            vectormp[0] = MediaPlayer.create(this, R.raw.dusttodust);
+            vectormp[1] = MediaPlayer.create(this, R.raw.sabbuttre);
+            vectormp[2] = MediaPlayer.create(this, R.raw.lomalodeserbueno);
+            posicion = 0;
+            btnplay_pause.setBackgroundResource(R.drawable.play);
+            iv.setBackgroundResource(R.drawable.blackalbum);
+            Toast.makeText(this, "Stop", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public  void  repetir(View view){
+        if (repetir == 1){
+            btn_repetir.setBackgroundResource(R.drawable.repeats);
+            vectormp[posicion].setLooping(false);
+            repetir = 2;
+            Toast.makeText(this, "No repetir", Toast.LENGTH_LONG).show();
+        }else{
+            btn_repetir.setBackgroundResource(R.drawable.norepeat);
+            vectormp[posicion].setLooping(true);
+            repetir = 1;
+            Toast.makeText(this, "Repetor", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public  void siguiente(View view){
+        if (posicion < vectormp.length -1){
+            if (vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+                posicion++;
+                vectormp[posicion].start();
+                if (posicion == 0){iv.setImageResource(R.drawable.queenofthemurderscene);}
+                else if (posicion == 1) {iv.setImageResource(R.drawable.blackalbum);}
+                else{iv.setImageResource(R.drawable.lomalodeserbueno);}
+            }else{
+                posicion++;
+                if (posicion == 0){iv.setImageResource(R.drawable.queenofthemurderscene);}
+                else if (posicion == 1) {iv.setImageResource(R.drawable.blackalbum);}
+                else{iv.setImageResource(R.drawable.lomalodeserbueno);}
+            }
+        }else{
+            Toast.makeText(this, "No hay canciones", Toast.LENGTH_LONG).show();
+        }
     }
 }
