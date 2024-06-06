61d4b034500c0d281f162c55cb1a7a9a47ab6a74
RafaelMurrieta <r7121005846@gmail.com>
2024-06-05 19:36:56 -0600

    Arreglo de boton anterior

--- app/src/main/java/com/murrydev/reproductor/MainActivity.java
+++ app/src/main/java/com/murrydev/reproductor/MainActivity.java
@@ -16,12 +16,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button btnplay_pause, btn_repetir;
-
     MediaPlayer mp;
     ImageView iv;
     int repetir = 2, posicion = 0;
-    MediaPlayer vectormp[] = new MediaPlayer[3];
-
+    private MediaPlayer[] vectormp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,24 +31,28 @@ public class MainActivity extends AppCompatActivity {
         btn_repetir = findViewById(R.id.repeat);
         iv = findViewById(R.id.cover);
 
-        vectormp[0] = MediaPlayer.create(this, R.raw.dusttodust);
-        vectormp[1] = MediaPlayer.create(this, R.raw.sabbuttre);
-        vectormp[2] = MediaPlayer.create(this, R.raw.lomalodeserbueno);
+        inicializarMediaPlayer();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
+
+    private void inicializarMediaPlayer() {
+        vectormp = new MediaPlayer[3];
+        vectormp[0] = MediaPlayer.create(this, R.raw.dusttodust);
+        vectormp[1] = MediaPlayer.create(this, R.raw.sabbuttre);
+        vectormp[2] = MediaPlayer.create(this, R.raw.lomalodeserbueno);
+    }
+
     public void PLayPause(View view){
         if (vectormp[posicion].isPlaying()){
             vectormp[posicion].pause();
             btnplay_pause.setBackgroundResource(R.drawable.play);
             Toast.makeText(this, "Pause", Toast.LENGTH_LONG).show();
-        }else{
+        } else {
             vectormp[posicion].start();
             btnplay_pause.setBackgroundResource(R.drawable.pause);
             Toast.makeText(this, "Play", Toast.LENGTH_LONG).show();
@@ -60,9 +62,8 @@ public class MainActivity extends AppCompatActivity {
     public void stop(View view){
         if (vectormp[posicion] != null){
             vectormp[posicion].stop();
-            vectormp[0] = MediaPlayer.create(this, R.raw.dusttodust);
-            vectormp[1] = MediaPlayer.create(this, R.raw.sabbuttre);
-            vectormp[2] = MediaPlayer.create(this, R.raw.lomalodeserbueno);
+            liberarMediaPlayer();
+            inicializarMediaPlayer();
             posicion = 0;
             btnplay_pause.setBackgroundResource(R.drawable.play);
             iv.setBackgroundResource(R.drawable.blackalbum);
@@ -70,37 +71,75 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public  void  repetir(View view){
+    private void liberarMediaPlayer() {
+        for (int i = 0; i < vectormp.length; i++) {
+            if (vectormp[i] != null) {
+                vectormp[i].release();
+                vectormp[i] = null;
+            }
+        }
+    }
+
+    public void repetir(View view){
         if (repetir == 1){
             btn_repetir.setBackgroundResource(R.drawable.repeats);
             vectormp[posicion].setLooping(false);
             repetir = 2;
             Toast.makeText(this, "No repetir", Toast.LENGTH_LONG).show();
-        }else{
+        } else {
             btn_repetir.setBackgroundResource(R.drawable.norepeat);
             vectormp[posicion].setLooping(true);
             repetir = 1;
-            Toast.makeText(this, "Repetor", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Repetir", Toast.LENGTH_LONG).show();
         }
     }
 
-    public  void siguiente(View view){
-        if (posicion < vectormp.length -1){
-            if (vectormp[posicion].isPlaying()){
-                vectormp[posicion].stop();
-                posicion++;
-                vectormp[posicion].start();
-                if (posicion == 0){iv.setImageResource(R.drawable.queenofthemurderscene);}
-                else if (posicion == 1) {iv.setImageResource(R.drawable.blackalbum);}
-                else{iv.setImageResource(R.drawable.lomalodeserbueno);}
-            }else{
-                posicion++;
-                if (posicion == 0){iv.setImageResource(R.drawable.queenofthemurderscene);}
-                else if (posicion == 1) {iv.setImageResource(R.drawable.blackalbum);}
-                else{iv.setImageResource(R.drawable.lomalodeserbueno);}
-            }
-        }else{
+    public void siguiente(View view){
+        if (posicion < vectormp.length - 1) {
+            vectormp[posicion].stop();
+            liberarMediaPlayer();
+            inicializarMediaPlayer();
+            posicion++;
+            vectormp[posicion].start();
+            setImageResource(posicion);
+        } else {
             Toast.makeText(this, "No hay canciones", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void before(View view){
+        if (posicion > 0) {
+            vectormp[posicion].stop();
+            liberarMediaPlayer();
+            inicializarMediaPlayer();
+            posicion--;
+            vectormp[posicion].start();
+            setImageResource(posicion);
+        } else {
+            Toast.makeText(this, "No hay canciones anteriores", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void setImageResource(int posicion) {
+        switch (posicion) {
+            case 0:
+                iv.setImageResource(R.drawable.queenofthemurderscene);
+                break;
+            case 1:
+                iv.setImageResource(R.drawable.blackalbum);
+                break;
+            case 2:
+                iv.setImageResource(R.drawable.lomalodeserbueno);
+                break;
+            default:
+                iv.setImageResource(R.drawable.queenofthemurderscene);
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        liberarMediaPlayer();
     }
 }
